322c05df54dd523526a4c50a55e09d86f827f60e
kdnth <kadenconradt@gmail.com>
2026-07-30 23:26:47 -0400

    chore: initial commit. vue scaffold with tailwindcss

---
+++ src/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import HomeHero from '@/components/HomeHero.vue'
+</script>
+
+<template>
+  <HomeHero />
+</template>
---
+++ src/components/HomeHero.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="white w-full">
+    <div class="p-8 flex flex-col items-center justify-center gap-4">
+      <img
+        class="max-w-48"
+        src="@/assets/lg-logo.png"
+        alt="Square logo with name Kaden Thrower and artistic representation of laptop"
+      />
+    </div>
+  </div>
+</template>
