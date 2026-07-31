a8cfdc0cb566eb371f81312ed2f1cc6f22d31428
kdnth <kadenconradt@gmail.com>
2026-07-31 02:17:32 -0400

    feat: home view with hero, about, projects, and footer

--- src/HomeView.vue
+++ src/HomeView.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts">
+import AboutSection from '@/components/AboutSection.vue'
 import HomeHero from '@/components/HomeHero.vue'
+import ProjectsSection from '@/components/ProjectsSection.vue'
+import SiteFooter from '@/components/SiteFooter.vue'
 </script>
 
 <template>
-  <HomeHero />
+  <div class="flex min-h-screen flex-col">
+    <HomeHero />
+    <main class="flex-1">
+      <AboutSection />
+      <ProjectsSection />
+    </main>
+    <SiteFooter />
+  </div>
 </template>
