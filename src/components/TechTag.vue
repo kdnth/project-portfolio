d63d577b8021f36af33d88190aca4b6855451a28
kdnth <kadenconradt@gmail.com>
2026-07-31 02:14:21 -0400

    feat: tech tag for ProjectPreviewCard

---
+++ src/components/TechTag.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  label: string
+}>()
+</script>
+
+<template>
+  <span
+    class="rounded-md border border-line bg-ink px-2.5 py-1 text-xs font-medium tracking-wide text-fog"
+  >
+    {{ label }}
+  </span>
+</template>
